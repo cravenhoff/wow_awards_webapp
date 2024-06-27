d0153bb8d42542d2254da0514e0ca03c216600c2
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2024-06-27 16:23:04 +1000

    Added the 'welcome' root/index route

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3033;
 
+// Welcome route (index)
+app.get('/', (req, res) => {
+    res.send('Welcome to the Papua New Guinea Westpac Outsanding Women Awards Portal, the home of incredible women doing incredible things.');
+    console.log('Welcome route hit');
+});
+
 app.listen(port, () => {
     console.log(`Server running on ${port}.`);
 });
